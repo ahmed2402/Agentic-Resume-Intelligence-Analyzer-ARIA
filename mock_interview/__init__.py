f14a8e546718affff01d5c63352650e0022271a9
Amna Faisal <amnafaisal1305@gmail.com>
2025-10-21 14:35:39 +0500

    module 4 start

---
+++ mock_interview/__init__.py
@@ -0,0 +1,7 @@
+"""
+Mock Interview Analyzer Module
+Provides AI-powered analysis of interview responses
+"""
+
+__version__ = "1.0.0"
+__author__ = "ARIA Team"
---
+++ mock_interview/core/__init__.py
@@ -0,0 +1,8 @@
+"""
+Core components for Mock Interview Analyzer
+"""
+
+from .audio_processor import AudioProcessor
+from .interview_analyzer import InterviewAnalyzer
+
+__all__ = ['AudioProcessor', 'InterviewAnalyzer']
