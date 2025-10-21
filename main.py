53b130b229d8c70ef1fcc3c47398114336e164bf
Amna Faisal <amnafaisal1305@gmail.com>
2025-10-21 15:56:35 +0500

    module 4

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ import pprint # Add this import
 import os
 import sys
 import streamlit as st
-from ui.resume_match_ui import show_resume_match_ui
-from ui.ats_checker_ui import show_ats_checker_ui
-from ui.interview_prep_ui import show_interview_prep_ui
+# from ui.resume_match_ui import show_resume_match_ui
+# from ui.ats_checker_ui import show_ats_checker_ui
+# from ui.interview_prep_ui import show_interview_prep_ui
+from ui.mock_interview_ui import show_mock_interview_ui
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "."))
 if project_root not in sys.path:
@@ -24,13 +25,16 @@ st.set_page_config(page_title="ARIA – Agentic Resume Intelligence Analyzer", l
 st.title("🤖 ARIA – Agentic Resume Intelligence Analyzer")
 
 # Create tabs for each module
-tabs = st.tabs(["📄 Resume Matcher", "📊 ATS Checker", "💬 Interview Prep Chatbot"])
+tabs = st.tabs(["📄 Resume Matcher", "📊 ATS Checker", "💬 Interview Prep Chatbot", "🎤 Mock Interview Analyzer"])
+
+# with tabs[0]:
+#     show_resume_match_ui()
+
+# with tabs[1]:
+#     show_ats_checker_ui()
+
+# with tabs[2]:
+    # show_interview_prep_ui()
 
 with tabs[0]:
-    show_resume_match_ui()
-
-with tabs[1]:
-    show_ats_checker_ui()
-
-with tabs[2]:
-    show_interview_prep_ui()
+    show_mock_interview_ui()
